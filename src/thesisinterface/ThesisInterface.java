949162ef07a420783d2dc709cd4217633ba35e92
Marina Athanasouli <mathanasouli@gmail.com>
2018-09-01 10:08:02 +0300

    Multiple Output

--- src/thesisinterface/ThesisInterface.java
+++ src/thesisinterface/ThesisInterface.java
@@ -12,10 +12,14 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import thesisinterface.VectorRepresentation.OneDimensional.AtomicNumberRepresentation;
+import thesisinterface.VectorRepresentation.OneDimensional.ElectronIonRepresentation;
+import thesisinterface.VectorRepresentation.OneDimensional.IntegerRepresentation;
+import thesisinterface.VectorRepresentation.OneDimensional.PairedNumeric;
+import thesisinterface.VectorRepresentation.OneDimensional.RealNumberRepresentation;
 
 /**
  *
- * @author marin
+ * @author marina
  */
 public class ThesisInterface {
 
@@ -28,10 +32,20 @@ public class ThesisInterface {
         Pattern fastaHeader = Pattern.compile("^>.*");
 
         File toReadFile = new File("D:\\Marina\\Documents\\MSc DataSets\\Comparison14\\InsectUCNEsSurrogates.fas");
-        File toWriteFile = new File("D:\\Marina\\Documents\\MSc DataSets\\Comparison14\\AtomicNumReprInsectUCNEsSurr.txt");
+        
+        File outputAtomicReprFile = new File("D:\\Marina\\Documents\\MSc DataSets\\Comparison14\\AtomicNumReprInsectUCNEsSurr.txt");
+        File outputElectronReprFile = new File("D:\\Marina\\Documents\\MSc DataSets\\Electron Ion Representation\\Comparison14\\AtomicNumReprInsectUCNEsSurr.txt");
+        File outputPairedReprFile = new File("D:\\Marina\\Documents\\MSc DataSets\\Paired Numeric Representation\\Comparison14\\AtomicNumReprInsectUCNEsSurr.txt");
+        File outputIntegerReprFile = new File("D:\\Marina\\Documents\\MSc DataSets\\Integer Number Representation\\Comparison14\\AtomicNumReprInsectUCNEsSurr.txt");
+        File outputRealReprFile = new File("D:\\Marina\\Documents\\MSc DataSets\\Real Number Representation\\Comparison14\\AtomicNumReprInsectUCNEsSurr.txt");
 
+        
         try (Scanner readDataFile = new Scanner(new FileReader(toReadFile));
-                FileWriter outputReprFile = new FileWriter(toWriteFile)) {
+                FileWriter outputFile1 = new FileWriter(outputAtomicReprFile);
+                FileWriter outputFile2 = new FileWriter(outputElectronReprFile);
+                FileWriter outputFile3 = new FileWriter(outputPairedReprFile);
+                FileWriter outputFile4 = new FileWriter(outputIntegerReprFile);
+                FileWriter outputFile5 = new FileWriter(outputRealReprFile)) {
 
             while (readDataFile.hasNextLine()) {
 
@@ -39,17 +53,24 @@ public class ThesisInterface {
 
                 if (input.matches(fastaHeader.pattern())) {
 
-                    outputReprFile.write(input + "\n");
+                    outputFile1.write(input + "\n");
+                    outputFile2.write(input + "\n");
+                    outputFile3.write(input + "\n");
+                    outputFile4.write(input + "\n");
+                    outputFile5.write(input + "\n");
 
                 } else {
 
-                    AtomicNumberRepresentation.atomicNumberRepresentation(outputReprFile, input);
-
+                    AtomicNumberRepresentation.atomicNumberRepresentation(outputFile1, input);
+                    ElectronIonRepresentation.electronIonRepresentation(outputFile2, input);
+                    PairedNumeric.pairedNumericRepresentation(outputFile3, input);
+                    IntegerRepresentation.integerNumberRepresentation(outputFile4, input);
+                    RealNumberRepresentation.realNumberRepresentation(outputFile5, input);
                 }
             }
         }
 
-        System.out.println("Now closing");
+        System.out.println("Closing");
     }
 
     public static boolean checkRepresentationResult() {
