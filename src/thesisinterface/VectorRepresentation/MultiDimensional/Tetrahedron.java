0e0112fb425bcae844a8d0840d74aff29c49bd50
George G <ggianna@iit.demokritos.gr>
2019-02-12 12:15:42 +0200

    - Fixed representation using all values.

--- src/thesisinterface/VectorRepresentation/MultiDimensional/Tetrahedron.java
+++ src/thesisinterface/VectorRepresentation/MultiDimensional/Tetrahedron.java
@@ -9,6 +9,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import thesisinterface.VectorRepresentation.BaseClasses.BaseFeatureVector;
 import thesisinterface.VectorRepresentation.BaseClasses.BaseSymbolSequence;
 import thesisinterface.VectorRepresentation.ISymbolSequence;
 import thesisinterface.VectorRepresentation.OneDimensional.RealNumberRepresentation;
@@ -27,44 +29,48 @@ public class Tetrahedron extends MultipleValueRepresentation {
     public void assignValues() {
        
        numValues.put("G", getMultipleValueList( -(Math.sqrt(2.0)/3)));
-       numValues.put("G", getMultipleValueList(-(Math.sqrt(6.0)/3)));
-       numValues.put("G", getMultipleValueList(-(1.0/3.0)));
+       numValues.get("G").addAll(getMultipleValueList(-(Math.sqrt(6.0)/3)));
+       numValues.get("G").addAll(getMultipleValueList(-(1.0/3.0)));
        numValues.put("A", getMultipleValueList(0.0));
-       numValues.put("A", getMultipleValueList(0.0));
-       numValues.put("A", getMultipleValueList(1.0));
+       numValues.get("A").addAll(getMultipleValueList(0.0));
+       numValues.get("A").addAll(getMultipleValueList(1.0));
        numValues.put("C", getMultipleValueList(-(Math.sqrt(2.0)/3)));
-       numValues.put("C", getMultipleValueList(Math.sqrt(6.0)/3));
-       numValues.put("C", getMultipleValueList(-(1.0/3.0)));
+       numValues.get("C").addAll(getMultipleValueList(Math.sqrt(6.0)/3));
+       numValues.get("C").addAll(getMultipleValueList(-(1.0/3.0)));
        numValues.put("T", getMultipleValueList(2*(Math.sqrt(2.0)/3)));
-       numValues.put("T", getMultipleValueList(0.0));
-       numValues.put("T", getMultipleValueList(-(1.0/3.0)));
+       numValues.get("T").addAll(getMultipleValueList(0.0));
+       numValues.get("T").addAll(getMultipleValueList(-(1.0/3.0)));
        
        calculateVectorDimensions();
        
     }
     
     @Override
-        public void calculateVectorDimensions(){
-        
-        
-            // For each symbol in sequence
-            for (int iSymbolCnt = 0; iSymbolCnt < sequence.size(); iSymbolCnt++) {
-                // Determine dimension name
-                int sDimensionName = iSymbolCnt + 1;
-                // Assign the corresponding value from the numValues key to the feature
-                put(sDimensionName, numValues.get(sequence.getSymbolAt(iSymbolCnt)));
-            }            
+    public void calculateVectorDimensions()
+    {
+        // For each symbol in sequence
+        for (int iSymbolCnt = 0; iSymbolCnt < sequence.size(); iSymbolCnt++) {
+            // Determine dimension name
+            int sDimensionName = iSymbolCnt + 1;
+            // Assign the corresponding value from the numValues key to the feature
+            put(sDimensionName, numValues.get(sequence.getSymbolAt(iSymbolCnt)));
+        }
     }
         
-        public static void tetrahedronRepresentation(FileWriter outputFile, String inputSequence) throws IOException {
-
+    public static MultipleValueRepresentation tetrahedronRepresentation(String inputSequence) throws IOException {
         BaseSymbolSequence inputSeq = new BaseSymbolSequence(inputSequence);
-        //TreeMap
         Tetrahedron tetrahedroRepr = new Tetrahedron(inputSeq);
-
+        tetrahedroRepr.assignValues();
         tetrahedroRepr.calculateVectorDimensions();
 
-        outputFile.write(tetrahedroRepr.toString() + "\n");
-        outputFile.write(tetrahedroRepr.getDimensionNames() + "\n");
+        return tetrahedroRepr;
     }
+//
+//    public static void main(String[] args) {
+//        System.out.println("The representation for the string AGTC is...");
+//        Tetrahedron t = new Tetrahedron(new BaseSymbolSequence("AGTC"));
+//        t.calculateVectorDimensions();
+//
+//        System.out.println();
+//    }
 }
